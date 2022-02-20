09db4c4dec19ad2d89bc82722b4738b9a14671e0
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-02-20 12:41:09 +0300

    hero section

--- components/Home.vue
+++ components/Home.vue
@@ -1,13 +1,96 @@
-<template>
-  <h1>text test component</h1>
-</template>
+<script setup lang="ts">
+import {ref} from 'vue';
 
-<script>
-export default {
-   name:"index"
-}
+useMeta({
+      title: 'Home Page'
+    })
+
 </script>
 
-<style>
+
+<template lang="pug">
+
+.container
+  .header-container
+    .wrapper
+      header
+        .hero-content
+          h1 Hi, I'm Nyll 
+          p I design beautiful Full Stack Web Applications
+          a(href="#") Contact Me 
+
+        .hero-image
+          img(src="~/public/Nyllde.png" width="500")
+
+      //- .social-icons
+      //-   img
+
+</template>
+
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  color:white;
+
+  background: url('~/public/citybg.jpg') #0006;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-blend-mode: color;
+
+  .header-container {
+    .wrapper {
+      width: 1200px;
+      margin: 0 auto;
+      header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .hero-content {
+          h1 {
+            font-style: normal;
+            font-weight: 800;
+            font-size: 4em;
+            line-height: 2em;
+          }
+
+          p {
+            font-size: 1.8em;
+            line-height: 1.5em;
+            color: #91d4d8;
+            margin-bottom: .8em
+          }
+
+          a {
+            text-decoration: none;
+            font-size: 1em;
+            color: white;
+            padding: .8em 2.5em;
+            background: rgb(159, 56, 255);
+            color: white;
+            border-radius: 10px;
+            transition: .3s;
+            display: inline-block;
+
+            &:hover {
+              transform: translateY(-8px)
+            }
+          }
+        }
+        .hero-image {
+          img {
+            border-radius: 3em;
+          }
+        }
+      }
+    }
+  }
+}
 
 </style>
