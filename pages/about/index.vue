76be85f19ae82e4c836ac026918c52eca00b2b96
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-02-20 14:44:11 +0300

    coming soon responsive page

---
+++ pages/about/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+h1 Coming Soon
+</template>
+
+<style lang="sass" scoped>
+
+h1
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100vh
+  width: 100%
+  text-align: center
+  font-size: 8em
+
+  @media only screen and (max-width: 992px) and (orientation: portrait)
+    font-size: 4em
+  
+</style>
